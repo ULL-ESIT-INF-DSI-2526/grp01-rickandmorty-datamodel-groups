df865c22eee45d1e19ebe595f6265233dd5a8f00
gabmartinbr <alu0101539157@ull.edu.es>
2026-03-19 20:28:47 +0000

    fase 3 done

--- src/interfaces/IEsquema.ts
+++ src/interfaces/IEsquema.ts
@@ -4,6 +4,9 @@ import { IInventoJSON } from "./IInventoJSON.js";
 import { IPersonajeJSON } from "./IPersonajeJSON.js";
 import { IPlanetaJSON } from "./IPlanetaJSON.js";
 
+/**
+ * interfaz que define el esquema que aceotara el json
+ */
 export interface IEsquema {
   personajes: IPersonajeJSON[];
   dimensiones: IDimension[];
