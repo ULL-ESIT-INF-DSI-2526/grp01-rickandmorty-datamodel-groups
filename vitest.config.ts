82f61ca5cd2afa7c870864ea8055a5ecfdbc972b
unknown <alu0101539157@ull.edu.es>
2026-03-22 16:34:20 +0000

    config files fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    // 1. Cambiamos 'src' por 'tests' para que busque en la carpeta correcta
     include: ["tests/**/*.{test,spec}.ts"],
-
-    // 2. Mantenemos las exclusiones por seguridad
     exclude: ["**/node_modules/**", "**/dist/**"],
+    
+    coverage: {
+      provider: 'v8', 
+      exclude: [
+        "**/node_modules/**",
+        "**/dist/**",
+        "src/index.ts", 
+        "tests/**"
+      ],
+      include: ["src/**"],
+    },
   },
 });
